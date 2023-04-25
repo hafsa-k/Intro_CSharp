491605959c868c1a882e34d54955f78e80d9e69c
Hafsa Khallouk <hafsa-k@hotmail.com>
2023-04-25 13:16:52 +0200

    age et convert

--- Demo_operateur/Program.cs
+++ Demo_operateur/Program.cs
@@ -4,8 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Test Git");
+            #region opérateur deplacement de bits 
+               /* int i = 1; //0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+
+                i = i << 4;
+
+                Console.WriteLine(i);*/
+            #endregion
+
+            #region opérateur ternaire 
+                Console.WriteLine("Veuillez indiquer votre âge : ");
+                int age;
+                bool is_convert = int.TryParse(Console.ReadLine(), out age);
+                if (is_convert)
+                {
+                    /* placement d'un if. toujours dans l'ordre suivant, vrai et puis faux */
+                    string status = (age < 18) ? "mineur" : "Majeur";
+                    Console.WriteLine($"Vous êtes {status}. ");
+                }
+                else {
+                    Console.WriteLine("Apprend à lire ?!");
+                }
+
+            #endregion
         }
     }
 }
